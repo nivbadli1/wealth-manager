b272c9cc8e6be3ff7171e5de9f0ca9c34c26ddb7
niv.badli <niv.badli@helloheart.com>
2025-08-18 02:03:55 +0300

    Fix empty interface errors by using type aliases

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -2,11 +2,7 @@ import * as React from "react"
 
 import { cn } from "@/lib/utils"
 
-export interface InputProps
-  extends React.InputHTMLAttributes<HTMLInputElement> {
-  // This interface extends the base input props without adding new properties
-  // eslint-disable-next-line @typescript-eslint/no-empty-object-type
-}
+export type InputProps = React.InputHTMLAttributes<HTMLInputElement>
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
   ({ className, type, ...props }, ref) => {
